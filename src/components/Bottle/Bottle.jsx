48323c52592ecdc22963fa7390983ff60cbd2fd8
sojibdasjr <contact.sojibdas@gmail.com>
2024-02-16 22:34:21 +0600

    display cart

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Bottle.css";
 
-const Bottle = ({ bottle }) => {
-  console.log(bottle);
+const Bottle = ({ bottle, handlePerchase }) => {
   const { category, name, seller, price, img } = bottle;
   return (
     <div className="bottle">
       <h4>Name : {name}</h4>
       <img src={img} alt="" />
       <h5>Price : $ {price}</h5>
+      <button onClick={() => handlePerchase(bottle)}>Purchase</button>
     </div>
   );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,17 +4,33 @@ import "./Bottles.css";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
+  const [purchase, setPurchase] = useState([]);
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       .then((data) => setBottles(data));
   }, []);
+  // events handler for perchase
+  const handlePerchase = (product) => {
+    const newPurchase = [...purchase, product];
+    setPurchase(newPurchase);
+  };
   return (
     <div>
       <h4>Bottles Here : {bottles.length}</h4>
+      {/* // purchase */}
+      <div className="cart_container">
+        <h2>Cart</h2>
+        <h4 className="cart"> {purchase.length}</h4>
+      </div>
+      {/* // products */}
       <div className="bottle_Container">
         {bottles.map((bottle) => (
-          <Bottle key={bottle.id} bottle={bottle}></Bottle>
+          <Bottle
+            key={bottle.id}
+            handlePerchase={handlePerchase}
+            bottle={bottle}
+          ></Bottle>
         ))}
       </div>
     </div>
